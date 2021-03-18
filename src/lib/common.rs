dd417b2fb906b07372ad1ddbe55019fd04cb8f8f
Zola Gonano <zolagonano@protonmail.com>
2021-03-18 13:47:46 -0600

    Add file management functions

--- src/lib/common.rs
+++ src/lib/common.rs
@@ -1,4 +1,7 @@
 use regex::Regex;
+use std::env::current_dir;
+use std::fs::{create_dir_all, read_dir, read_to_string, write};
+use std::path::Path;
 
 pub fn split_header_from_markdown(markdown: &str) -> Vec<String> {
     let re = Regex::new(r"\A---\n((.|\n)*?)---\n((.|\n)*)").expect("There is a problem in regex");
@@ -10,9 +13,48 @@ pub fn split_header_from_markdown(markdown: &str) -> Vec<String> {
     vec![caps[1].to_string(), caps[3].to_string()]
 }
 
+pub fn read_file(path: &str) -> String {
+    let path = Path::new(path);
+    read_to_string(path)
+        .expect("could not read this file")
+        .parse()
+        .expect("could not parse this file")
+}
+
+pub fn list_dir(path: &str) -> Result<Vec<String>, &'static str> {
+    let path = Path::new(path);
+    if path.is_dir() {
+        let mut file_list: Vec<String> = Vec::new();
+        for file in read_dir(path).expect("could not read this directory") {
+            file_list.push(file.unwrap().file_name().into_string().unwrap());
+        }
+
+        Ok(file_list)
+    } else {
+        Err("not a directory")
+    }
+}
+
+pub fn make_dirs(dirs: &Vec<String>) {
+    for dir in dirs {
+        create_dir_all(dir).expect("could not create directory");
+    }
+}
+
+pub fn pwd() -> String {
+    current_dir()
+        .expect("could not get current directory")
+        .display()
+        .to_string()
+}
+
+pub fn write_file(path: &str, text: &str) {
+    let path = Path::new(path);
+    write(path, text).expect("could not write file");
+}
+
 #[cfg(test)]
 mod tests {
-
     use super::split_header_from_markdown;
     #[test]
     fn split_header_test() {
