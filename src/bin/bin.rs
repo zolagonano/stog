b8825326ccf31f6e55a405938a16b30da4ac3f73
Zola Gonano <zolagonano@protonmail.com>
2021-04-04 07:16:52 -0600

    Build initialize and build subcommands and add argument parser

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,3 +1,165 @@
-fn main(){
-    println!("Hello STOG");
+use clap::{App, Arg, SubCommand};
+use lib::{common, config, header_parser, md_parser, templator};
+use md5::compute as md5_compute;
+use std::collections::HashMap;
+
+fn main() {
+    let matches = App::new("STOG: Static Blog Generator")
+        .version("0.1.0")
+        .author("Zola Gonano <zolagonano@protonmail.com>")
+        .about("Generates a static blog from markdown files.")
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Initialize blog directory")
+                .arg(Arg::with_name("blog_name").required(true)),
+        )
+        .subcommand(SubCommand::with_name("build").about("Builds the blog files"))
+        .get_matches();
+
+    if matches.is_present("init") {
+        let blog_name = matches
+            .subcommand_matches("init")
+            .unwrap()
+            .value_of("blog_name")
+            .unwrap();
+
+        initialize(blog_name);
+    } else if matches.is_present("build") {
+        build();
+    }
+}
+
+fn initialize(blog_name: &str) {
+    let dir_list = vec![
+        String::from(blog_name),
+        format!("{}/_posts", blog_name),
+        format!("{}/_templates", blog_name),
+        format!("{}/public", blog_name),
+    ];
+
+    common::make_dirs(&dir_list);
+
+    let config_template_file = include_str!("../init_files/config.toml");
+    let config = templator::InitConfig::new(config::Config::get_defaults())
+        .template_text(&config_template_file);
+
+    let write_list: &[&(String, &str)] = &[
+        &(format!("{}/config.toml", blog_name), config.as_str()),
+        &(
+            format!("{}/_posts/lorem1.md", blog_name),
+            include_str!("../init_files/posts/lorem1.md"),
+        ),
+        &(
+            format!("{}/_posts/lorem2.md", blog_name),
+            include_str!("../init_files/posts/lorem2.md"),
+        ),
+        &(
+            format!("{}/_templates/index.html", blog_name),
+            include_str!("../init_files/templates/index.html"),
+        ),
+        &(
+            format!("{}/_templates/post.html", blog_name),
+            include_str!("../init_files/templates/post.html"),
+        ),
+        &(
+            format!("{}/_templates/atom.xml", blog_name),
+            include_str!("../init_files/templates/atom.xml"),
+        ),
+        &(
+            format!("{}/public/style.css", blog_name),
+            include_str!("../init_files/theme/style.css")
+        )
+    ];
+
+    for write_data in write_list {
+        common::write_file(&write_data.0, write_data.1);
+    }
+}
+
+fn get_post(config: config::Config, post_path: String) -> templator::Post {
+    let post_file = common::read_file(&post_path);
+    let (header, body) = common::split_header_from_markdown(&post_file);
+
+    let config_headers = config.post_headers.clone();
+
+    let yaml_parser = header_parser::Yaml::new(config_headers, header);
+    let header = yaml_parser.parse();
+
+    let markdown_parser = md_parser::Html::new(&body);
+    let body = markdown_parser.parse();
+
+    let file_name = post_path.replace("_posts/", "").replace(".md", ".html");
+    let file_id = format!(
+        "{:x}",
+        md5_compute(format!("{:#?}{}{}", &header, &body, &post_path))
+    );
+
+    let metadata = templator::MetaData::new(file_name, file_id);
+
+    templator::Post::new(config, body, header, metadata).clone()
+}
+
+fn build() {
+    let current_dir = common::pwd();
+    let dir_list = common::list_dir(&current_dir).unwrap();
+
+    let is_blog_dir = dir_list.contains(&"config.toml".to_string())
+        && dir_list.contains(&"_posts".to_string())
+        && dir_list.contains(&"public".to_string())
+        && dir_list.contains(&"_templates".to_string());
+
+    if dir_list.contains(&"_build".to_string()){
+        common::rm_dir("_build");
+    }
+
+    if is_blog_dir {
+        let config_file = common::read_file("config.toml");
+        let config = config::Config::read_config(&config_file);
+
+        common::make_dirs(&vec![
+            String::from("_build"),
+            String::from("_build/posts"),
+            String::from("_build/public"),
+        ]);
+
+        common::copy_dir(&vec!["public"], "_build");
+
+        let posts: Vec<String> = common::list_dir("_posts")
+            .unwrap()
+            .iter()
+            .map(|item| format!("_posts/{}", item))
+            .collect();
+
+        let mut index_posts: Vec<templator::Post> = Vec::new();
+
+        let post_template_file = common::read_file("_templates/post.html");
+        for post in posts {
+            let template_post = get_post(config.clone(), post.clone());
+            common::write_file(
+                &format!(
+                    "_build/posts/{}",
+                    post.replace("_posts/", "").replace(".md", ".html")
+                ),
+                &template_post.template_text(&post_template_file),
+            );
+
+            index_posts.push(template_post.clone());
+        }
+
+        let index_template_file = common::read_file("_templates/index.html");
+        let template_index = templator::Index::new(&config, &index_posts);
+        common::write_file(
+            "_build/index.html",
+            &template_index.template_text(&index_template_file),
+        );
+
+        let feed_template_file = common::read_file("_templates/atom.xml");
+        let template_feed = templator::Feed::new(&config, &index_posts);
+        common::write_file(
+            "_build/atom.xml",
+            &template_feed.template_text(&feed_template_file),
+        );
+    } else {
+        eprintln!("Could not find necessary directories!!!");
+    }
 }
