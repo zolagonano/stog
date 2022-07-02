6271e144285d19c26ecc15abb9655f79bfad0123
Zola Gonano <zolagonano@protonmail.com>
2022-07-02 15:57:19 +0000

    return toml string of default config

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,5 +1,4 @@
 use serde_derive::{Deserialize, Serialize};
-use toml::from_str;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Config {
@@ -12,20 +11,22 @@ pub struct Config {
 
 impl Config {
     pub fn read_config(toml_text: &str) -> Config {
-        let config: Config = from_str(toml_text).expect("could not parse the config file");
+        let config: Config = toml::from_str(toml_text).expect("could not parse the config file");
         config
     }
-    pub fn get_defaults() -> Config {
-        Config {
+    pub fn get_defaults_string() -> String {
+        let default_config = Config {
             base_url: String::from(""),
             site_name: String::from("STOG"),
             site_author: String::from("Somebody"),
             site_description: String::from("generated with STOG"),
             post_headers: vec![
-                String::from("\"title\""),
-                String::from("\"date\""),
-                String::from("\"author\""),
+                String::from("title"),
+                String::from("date"),
+                String::from("author"),
             ],
-        }
+        };
+
+        toml::to_string_pretty(&default_config).unwrap()
     }
 }
