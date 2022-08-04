791596d3857ad2608c0529347dd197c90f7018bd
Zola Gonano <zolagonano@protonmail.com>
2022-08-04 13:55:53 +0000

    Use serde_yaml::Value instead of HashMap

--- src/lib/post_parser.rs
+++ src/lib/post_parser.rs
@@ -1,17 +1,14 @@
 use pulldown_cmark::{html::push_html, Options, Parser};
 use regex::Regex;
-use std::collections::HashMap;
 
 pub struct PostParser {
     markdown_text: String,
-    key_list: Vec<String>,
 }
 
 impl PostParser {
-    pub fn new(markdown_text: &str, key_list: &[String]) -> PostParser {
+    pub fn new(markdown_text: &str) -> PostParser {
         PostParser {
             markdown_text: markdown_text.to_string(),
-            key_list: key_list.to_vec(),
         }
     }
 
@@ -25,23 +22,11 @@ impl PostParser {
         (captures[1].to_string(), captures[3].to_string())
     }
 
-    pub fn parse_header(&self) -> HashMap<String, String> {
+    pub fn parse_header(&self) -> serde_yaml::Value {
         let yaml: serde_yaml::Value = serde_yaml::from_str(&self.seperate_header().0)
             .expect("Could not parse the yaml header");
 
-        let mut parsed_yaml: HashMap<String, String> = HashMap::new();
-
-        for key in &self.key_list {
-            parsed_yaml.insert(
-                key.to_string(),
-                yaml[key]
-                    .as_str()
-                    .map(|value| value.to_string())
-                    .unwrap_or_else(|| panic!("could not find key {} in your yaml header", key)),
-            );
-        }
-
-        parsed_yaml
+        yaml
     }
 
     pub fn parse_md(&self) -> String {
@@ -53,39 +38,5 @@ impl PostParser {
         push_html(&mut html, parser);
 
         html
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::PostParser;
-    use std::collections::HashMap;
-
-    const TEST_MD: &str = r#"---
-title: Testing title!
-date: 1970-1-1
----
-
-**Blog post goes here!**
-"#;
-
-    #[test]
-    fn test_parse_and_seperate_header() {
-        let parser = PostParser::new(TEST_MD, &["title".to_string(), "date".to_string()]);
-
-        let mut expected_result: HashMap<String, String> = HashMap::new();
-
-        expected_result.insert(String::from("title"), String::from("Testing title!"));
-        expected_result.insert(String::from("date"), String::from("1970-1-1"));
-
-        assert_eq!(parser.parse_header(), expected_result);
-    }
-
-    #[test]
-    fn tesst_parse_md() {
-        let parser = PostParser::new(TEST_MD, &["title".to_string(), "date".to_string()]);
-        let expected_result = "<p><strong>Blog post goes here!</strong></p>\n";
-
-        assert_eq!(parser.parse_md(), expected_result);
     }
 }
